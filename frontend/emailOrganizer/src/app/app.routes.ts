a015007a0675c4f5a1441c089bc982a087999141
Daniel Mora <danielleonardomora.04@gmail.com>
2025-04-09 10:51:54 -0500

    kanban board

--- frontend/emailOrganizer/src/app/app.routes.ts
+++ frontend/emailOrganizer/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EmailDashboardComponent } from './email-dashboard/email-dashboard.component';
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegisterComponent },
+  { path: 'dashboard', component: EmailDashboardComponent },
+  { path: 'kanban', component: KanbanBoardComponent }
+];
